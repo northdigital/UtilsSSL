d2e0b74514f780186cca9a0f01af306638cd63b7
northdigital <giotismail@yahoo.com>
2017-10-22 11:50:45 +0300

    fixed Netcape Comment extension

--- src/main/java/gr/northdigital/utilssl/IntermediateCertBuilder.java
+++ src/main/java/gr/northdigital/utilssl/IntermediateCertBuilder.java
@@ -1,6 +1,7 @@
 package gr.northdigital.utilssl;
 
 import org.bouncycastle.asn1.*;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
 import org.bouncycastle.asn1.x500.X500NameBuilder;
 import org.bouncycastle.asn1.x500.style.BCStyle;
 import org.bouncycastle.asn1.x509.BasicConstraints;
@@ -25,13 +26,12 @@ public class IntermediateCertBuilder {
     X500NameBuilder nameBuilder = new X500NameBuilder();
 
     nameBuilder.addRDN(BCStyle.C, name);
-    //nameBuilder.addRDN(BCStyle.O, "The Legion of the Bouncy Castle");
-    //nameBuilder.addRDN(BCStyle.OU, "Bouncy Intermediate Certificate");
-    //nameBuilder.addRDN(BCStyle.EmailAddress, "feedback-crypto@bouncycastle.org");
 
-    X509v3CertificateBuilder v3Bldr = new JcaX509v3CertificateBuilder(caCertificate, BigInteger.valueOf(2),
-      new Date(System.currentTimeMillis() - 1000L * 60 * 60 * 24 * 30), new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 30)),
-      nameBuilder.build(), pubKey);
+    long nowms = System.currentTimeMillis();
+    Date validFromDate = new Date(nowms - (1000L * 60 * 60 * 24 * 365 * 1));
+    Date validToDate = new Date(nowms + (1000L * 60 * 60 * 24 * 365 * 100));
+    X509v3CertificateBuilder v3Bldr = new JcaX509v3CertificateBuilder(
+      caCertificate, BigInteger.valueOf(2), validFromDate, validToDate, nameBuilder.build(), pubKey);
 
     JcaX509ExtensionUtils extUtils = new JcaX509ExtensionUtils();
 
@@ -45,12 +45,12 @@ public class IntermediateCertBuilder {
     Extension extension = new Extension(new ASN1ObjectIdentifier("2.16.840.1.113730.1.13"), false, netscapeCommentEncoded);
     v3Bldr.addExtension(extension);
 
-    X509CertificateHolder certHldr = v3Bldr.build(new JcaContentSignerBuilder("SHA1WithRSA").setProvider("BC").build(caPrivateKey));
+    X509CertificateHolder certHldr = v3Bldr.build(new JcaContentSignerBuilder("SHA256withRSA").setProvider("BC").build(caPrivateKey));
     X509Certificate x509Certificate = new JcaX509CertificateConverter().setProvider("BC").getCertificate(certHldr);
     x509Certificate.checkValidity(new Date());
     x509Certificate.verify(caCertificate.getPublicKey());
     PKCS12BagAttributeCarrier bagAttr = (PKCS12BagAttributeCarrier) x509Certificate;
-    //bagAttr.setBagAttribute(PKCSObjectIdentifiers.pkcs_9_at_friendlyName, new DERBMPString("Bouncy Intermediate Certificate"));
+    bagAttr.setBagAttribute(PKCSObjectIdentifiers.pkcs_9_at_friendlyName, new DERBMPString("End User Certificate"));
 
     return x509Certificate;
   }
